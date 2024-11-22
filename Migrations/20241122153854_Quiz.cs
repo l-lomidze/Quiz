e8bed86ae5bd4630dd40252d605f4786357e39e3
l-lomidze <l_lomidze2@cu.edu.ge>
2024-11-22 19:53:28 +0400

    first commit

---
+++ Migrations/20241122153854_Quiz.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz.Migrations
+{
+    /// <inheritdoc />
+    public partial class Quiz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    SubjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    MaximumCapacity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.SubjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentSubject",
+                columns: table => new
+                {
+                    EnrolledStudentsStudentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnrolledSubjectsSubjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubject", x => new { x.EnrolledStudentsStudentId, x.EnrolledSubjectsSubjectId });
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Students_EnrolledStudentsStudentId",
+                        column: x => x.EnrolledStudentsStudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Subjects_EnrolledSubjectsSubjectId",
+                        column: x => x.EnrolledSubjectsSubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "SubjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubject_EnrolledSubjectsSubjectId",
+                table: "StudentSubject",
+                column: "EnrolledSubjectsSubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentSubject");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+public class QuizContext : DbContext {
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) {
+        options.UseSqlite("Data source=Quiz.db");
+        base.OnConfiguring(options);
+    }
+}
+
+public class Student {
+    [Key]
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public List<Subject> EnrolledSubjects { get; set; } = new();
+}
+
+public class Subject {
+
+    [Key]
+    public int SubjectId { get; set; }
+    public string Title { get; set; }
+    public int MaximumCapacity { get; set; }
+    public List<Student> EnrolledStudents { get; set; } = new();
+}
+
+public class QuizRepository {
+    private QuizContext dbContext;
+
+    public QuizRepository() {
+        this.dbContext = new QuizContext();
+        dbContext.Database.EnsureCreated();
+    }
+
+    public int AddSubject(Subject subject) {
+        dbContext.Add(subject);
+        dbContext.SaveChanges();
+        return subject.SubjectId;
+    }
+
+    public int AddStudent(Student student) {
+        dbContext.Add(student);
+        dbContext.SaveChanges();
+        return student.StudentId;
+    }
+
+    public void EnrollStudentToSubject(int studentId, int subjectId) {
+        var subject = dbContext.Subjects
+            .Include(subject => subject.EnrolledStudents)
+            .FirstOrDefault(subject => subject.SubjectId == subjectId);
+        
+        var student = dbContext.Students
+                .FirstOrDefault(student => student.StudentId == studentId);
+
+        subject?.EnrolledStudents.Add(student);
+
+        dbContext.SaveChanges();
+    }
+
+    public List<Subject> GetAllSubjects() {
+        return dbContext.Subjects.ToList();
+    }
+
+    public List<Student>? GetStudentsForSubject(int subjectId) {
+        return dbContext.Subjects
+            .Include(subject => subject.EnrolledStudents)
+            .FirstOrDefault(subject => subject.SubjectId == subjectId)
+            ?.EnrolledStudents
+            ?.ToList();
+    }
+
+}
+
+public class Program {
+    private static QuizRepository repository = new();
+
+    public static void Main() {
+        var subject1 = new Subject() { Title = ".NET", MaximumCapacity = 20 };
+
+        var student1 = new Student() { Name = "John", EnrollmentDate = DateTime.Now };
+        var student2 = new Student() { Name = "Jane", EnrollmentDate = DateTime.Now };
+
+        repository.AddSubject(subject1);
+
+        repository.AddStudent(student1);
+        repository.AddStudent(student2);
+
+        repository.EnrollStudentToSubject(student1.StudentId, subject1.SubjectId);
+        repository.EnrollStudentToSubject(student2.StudentId, subject1.SubjectId);
+
+        student1.EnrolledSubjects.ForEach(subject => Console.WriteLine(subject.Title));
+
+        subject1.EnrolledStudents.ForEach(student => Console.WriteLine(student.Name));
+    }
+}
